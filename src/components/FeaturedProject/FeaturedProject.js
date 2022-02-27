676737473606b1258be7856709500c1cc0b5a61a
D20CM <scottbrown500@hotmail.com>
2022-02-27 00:09:09 +0000

    minor tweaks to styling of pitch area on mobile

--- src/components/FeaturedProject/FeaturedProject.js
+++ src/components/FeaturedProject/FeaturedProject.js
@@ -4,36 +4,39 @@ import image from "../../images/Screenshot.png";
 
 export default function FeaturedProject() {
   return (
-    <section className={css.project}>
-      <div className={css.projectLeft}>
-        <img className={css.projectImage} src={image} alt="something"></img>
-        <div className={css.projectLinks}>
-          <button className={css.projectLink}>Live demo</button>
-          <button className={css.projectLink}>View code</button>
+    <>
+      <section className={css.project}>
+        <div className={css.projectLeft}>
+          <img className={css.projectImage} src={image} alt="something"></img>
+          <div className={css.projectLinks}>
+            <button className={css.projectLink}>Live demo</button>
+            <button className={css.projectLink}>View code</button>
+          </div>
         </div>
-      </div>
-      <div className={css.projectRight}>
-        <h2 className={css.projectTitle}>Project Title</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo co
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo co
-        </p>
-        <div className={css.techIconsArea}>
-          <div className={css.techIcon}></div>
-          <div className={css.techIcon}></div>
-          <div className={css.techIcon}></div>
-          <div className={css.techIcon}></div>
-          <div className={css.techIcon}></div>
+        <div className={css.projectRight}>
+          <h2 className={css.projectTitle}>Project Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo co
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo co
+          </p>
+          <div className={css.techIconsArea}>
+            <div className={css.techIcon}></div>
+            <div className={css.techIcon}></div>
+            <div className={css.techIcon}></div>
+            <div className={css.techIcon}></div>
+            <div className={css.techIcon}></div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <hr className={css.projectSeperator} />
+    </>
   );
 }
