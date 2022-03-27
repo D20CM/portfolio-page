867ef525e129446b4aaf38c8d8587ab0846522b3
D20CM <scottbrown500@hotmail.com>
2022-03-27 13:52:07 +0100

    clear form on submit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,41 +3,44 @@ import emailjs from "emailjs-com";
 import css from "./contact.module.css";
 
 const Contact = () => {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [emailSent, setEmailSent] = useState(false);
   const form = useRef();
+  // useEffect(() => {
+  //   console.log("Name: ", name, "Email: ", email, "Message: ", message);
+  // }, [name, email, message]);
 
   const submit = (e) => {
     e.preventDefault();
 
-    // if (name && email && message) {
-    const serviceId = process.env.REACT_APP_SERVICE_ID;
-    const templateId = process.env.REACT_APP_TEMPLATE_ID;
-    const userId = process.env.REACT_APP_USER_ID;
+    if (name && email && message) {
+      const serviceId = process.env.REACT_APP_SERVICE_ID;
+      const templateId = process.env.REACT_APP_TEMPLATE_ID;
+      const userId = process.env.REACT_APP_USER_ID;
 
-    // const templateParams = {
-    //   name,
-    //   email,
-    //   message,
-    // };
+      // const templateParams = {
+      //   name,
+      //   email,
+      //   message,
+      // };
 
-    emailjs
-      .sendForm(serviceId, templateId, form.current, userId)
-      .then((result) => console.log(result))
-      .then((error) => console.log(error));
+      emailjs
+        .sendForm(serviceId, templateId, form.current, userId)
+        .then((result) => console.log(result))
+        .then((error) => console.log(error));
 
-    setEmailSent(true);
+      setEmailSent(true);
 
-    // setName("");
-    // setEmail("");
-    // setMessage("");
-    // setEmailSent(true);
-    // console.log("email has been sent ", process.env.REACT_APP_SERVICE_ID);
-    // } else {
-    //   alert("Please fill in all fields.");
-    // }
+      setName("");
+      setEmail("");
+      setMessage("");
+      // setEmailSent(true);
+      console.log("email has been sent ", process.env.REACT_APP_SERVICE_ID);
+    } else {
+      alert("Please fill in all fields.");
+    }
   };
 
   //   const isValidEmail = (email) => {
@@ -60,30 +63,32 @@ const Contact = () => {
           type="text"
           placeholder="Your Name"
           name="name"
+          value={name}
           className={css.nameInput}
-          // onChange={(e) => {
-          //   setName(e.target.value);
-          // }}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <label>Email</label>
         <input
           type="email"
           placeholder="Your email address"
           name="email"
+          value={email}
           className={css.emailInput}
-
-          // onChange={(e) => {
-          //   setEmail(e.target.value);
-          // }}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <label>Message</label>
 
         <textarea
           placeholder="Your message"
           name="message"
-          // onChange={(e) => {
-          //   setMessage(e.target.value);
-          // }}
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
           className={css.messageInput}
         ></textarea>
         <input type="submit" value="Send" className={css.submit} />
